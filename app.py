75d56b0b926d371ecc6996a01823f28a028f2773
edyeung4 <edyeung4@gmail.com>
2021-08-11 23:19:46 -0400

    refactored code

--- app.py
+++ app.py
@@ -15,44 +15,33 @@ def curr_conv():
 
     access_key = 'd7853e4ed0d5d37e8676'
 
-    # response = requests.get(f"https://free.currconv.com/api/v7/convert?q=USD_CAD&compact=ultra&apiKey={access_key}")
-
     if date.today() == api_call_date and curr_store is not None:
         return curr_store.json()['USD_CAD']
     else:
         curr_store = requests.get(f"https://free.currconv.com/api/v7/convert?q=USD_CAD&compact=ultra&apiKey={access_key}")
-
         return curr_store.json()['USD_CAD']
 
 @app.route('/post/', methods=['POST'])
 def post_something():
-    api_call_date = date.today()
-
+    usd_cad_conv = curr_conv()
     item_dict = {
-        'items' : [None]
+        'items' : []
     }
 
     item_list = request.get_json()['inline_shopping_results']
-    for item in item_list:
 
-        # replaces first item value in dict which was intialized to None
-        if item_dict['items'][0] == None:
-            item_dict['items'][0] = {
-                'item_pos' : item['position'],
-                'price' : [{'USD' : item['price']}, {'CAD' : f"${item['extracted_price']:.2f}" }]
-            }
-        else:
-            item_dict['items'].append({
-                'item_pos' : item['position'],
-                'price' : [{'USD' : item['price']}, {'CAD' : f"${item['extracted_price']*curr_conv():.2f}" }]
-            })
+    for item in item_list:
+        item_dict['items'].append({
+            'item_pos' : item['position'],
+            'price' : [{'USD' : item['price']}, {'CAD' : f"${item['extracted_price']*usd_cad_conv:.2f}" }]
+        })
     return item_dict
 
 
 # A welcome message to test our server
 @app.route('/')
 def index():
-    return "<h1>Cant see gay nibbas like anjan !!</h1>"
+    return "<h1>Welcome to price USD-CAD converter !!</h1>"
 
 
 if __name__ == '__main__':
