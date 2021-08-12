163721f59b198d7b823f461558c5ee469bba691f
edyeung4 <edyeung4@gmail.com>
2021-08-12 00:55:12 -0400

    refactored code

--- app.py
+++ app.py
@@ -3,39 +3,25 @@ from flask import Flask, request, jsonify
 from datetime import date
 app = Flask(__name__)
 
-global api_call_date
-global curr_store
-api_call_date = date.today()
-curr_store = None
-
-def curr_conv():
-    """ Currency converter api converts USD to CAD and Eur 
-        https://free.currencyconverterapi.com/ """
-    global curr_store, api_call_date
-    access_key = 'd7853e4ed0d5d37e8676'
-
-    if date.today() == api_call_date and curr_store is not None:
-        print('not api call')
-        return curr_store.json()
-    else:
-        # curr_store = requests.get(f"https://free.currconv.com/api/v7/convert?q=USD_CAD&compact=ultra&apiKey={access_key}")
-        curr_store = requests.get("https://free.currconv.com/api/v7/convert?q=USD_CAD&compact=ultra&apiKey=d7853e4ed0d5d37e8676")
-        print('api call')
-        return curr_store.json()
 
 @app.route('/post/', methods=['POST'])
 def post_something():
-    # usd_cad_conv = curr_conv()['USD_CAD']
+    """ POST request route handler makes api call to convert USD to CAD and returns 
+        JSON object of item and prices"""
+
+    # api call to https://free.currencyconverterapi.com/
     access_key = 'd7853e4ed0d5d37e8676'
     response = requests.get(f"https://free.currconv.com/api/v7/convert?q=USD_CAD&compact=ultra&apiKey={access_key}")
     usd_cad_conv = response.json()["USD_CAD"]
-    # print(usd_cad_conv)
+
     item_dict = {
         'items' : []
     }
 
+    # obtains JSON request body and filters for shopping results
     item_list = request.get_json()['inline_shopping_results']
 
+    # creates JSON response object with item and price conversion
     for item in item_list:
         item_dict['items'].append({
             'item_pos' : item['position'],
@@ -43,15 +29,9 @@ def post_something():
         })
     return item_dict
 
-
 # A welcome message to test our server
 @app.route('/')
 def index():
-    # usd_cad_conv = curr_conv()['USD_CAD']
-    # test = usd_cad_conv*2
-    # breakpoint()
-    # return [usd_cad_conv]
-    # return usd_cad_conv
     return "<h1>Welcome to price USD-CAD converter !!</h1>"
 
 
